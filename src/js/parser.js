f18c572d01b039dd76028bf8c4a364a10140e897
millermedeiros <miller@millermedeiros.com>
2012-02-22 02:13:16 -0200

    make sure descriptions are paragraphs. closes #10

--- src/js/parser.js
+++ src/js/parser.js
@@ -73,14 +73,15 @@ function getHeaderHashes(level){
 }
 
 function getDescription(mdown, fromIndex) {
-    var desc = mdown.substr(fromIndex)
-                .replace(/\r\n/g,'\n') // DOS to Unix
-                .replace(/\r/g,'\n') // Mac to Unix
-                .replace(/^\n+/g, '')
-                .split(/\n\n/)[0] //first paragraph
-                .replace(/\n+/, ' ');
+    var desc = mdown.substr(fromIndex);
+    desc = desc.replace(/^\n+/g, '').split(/\n\n/)[0]; //first paragraph
 
-    desc = showdown.parse(desc)
+    //check if line starts with a header, hr or code block. fixes #10
+    if ((/^(?:(?:[#=]+)|(?:[\-`\=]{3,})|(?: {4,}))/).test(desc)) {
+        return null;
+    }
+
+    desc = showdown.parse(desc.replace(/\n+/, ' '))
                     .replace(/<\/?p>/g, '') //remove paragraphs
                     .replace(/<\/?a[^>]*>/g, ''); //remove links since it breaks layout
     return desc;
